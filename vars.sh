dcb59502f6c7985e6ec78fe3d735758dcdab244d
Chris Rawnsley <casr@darkcog.com>
2014-01-04 13:10:27 +0000

    Add in a a separate swap drive.
    Related to GH-8

--- vars.sh
+++ vars.sh
@@ -2,5 +2,6 @@ NAME=centos65-x86_64
 TYPE=RedHat_64
 INSTALLER="./isos/CentOS-6.5-x86_64-minimal.iso"
 GUESTADDITIONS="./isos/VBoxGuestAdditions-4.3.4.iso"
-HDD="${HOME}/VirtualBox VMs/${NAME}/${NAME}.vmdk"
+HDD="${HOME}/VirtualBox VMs/${NAME}/main.vdi"
+HDD_SWAP="${HOME}/VirtualBox VMs/${NAME}/swap.vdi"
 NATNET=10.0.2.0/24
