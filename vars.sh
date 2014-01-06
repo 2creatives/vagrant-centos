9948c4a8134261fd620b38a6ca631a0294bd6097
Chris Rawnsley <casr@darkcog.com>
2014-01-06 00:21:17 +0000

    Upgrade Guest Additions version.

--- vars.sh
+++ vars.sh
@@ -1,7 +1,7 @@
 NAME=centos65-x86_64
 TYPE=RedHat_64
 INSTALLER="./isos/CentOS-6.5-x86_64-minimal.iso"
-GUESTADDITIONS="./isos/VBoxGuestAdditions-4.3.4.iso"
+GUESTADDITIONS="./isos/VBoxGuestAdditions-4.3.6.iso"
 HDD="${HOME}/VirtualBox VMs/${NAME}/main.vdi"
 HDD_SWAP="${HOME}/VirtualBox VMs/${NAME}/swap.vdi"
 NATNET=10.0.2.0/24
