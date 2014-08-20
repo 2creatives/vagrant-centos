b4727ea8e6f0376b62877fb829bc3104856bd49d
Chris Rawnsley <chris@2creatives.com>
2014-08-20 20:05:58 +0100

    Upgrade Guest Addition's version
    Closes GH-17

--- vars.sh
+++ vars.sh
@@ -1,7 +1,7 @@
 NAME=centos65-x86_64
 TYPE=RedHat_64
 INSTALLER="./isos/CentOS-6.5-x86_64-minimal.iso"
-GUESTADDITIONS="./isos/VBoxGuestAdditions-4.3.6.iso"
+GUESTADDITIONS="./isos/VBoxGuestAdditions_4.3.14.iso"
 HDD="${HOME}/VirtualBox VMs/${NAME}/main.vdi"
 HDD_SWAP="${HOME}/VirtualBox VMs/${NAME}/swap.vdi"
 NATNET=10.0.2.0/24
